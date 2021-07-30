8129eea329cdebbc4b8180227f4d903e4472a464
Miguel Nunez <71807189+iamcodefoxx@users.noreply.github.com>
2021-07-30 09:00:15 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
-  const amount = document.getElementById("amount");
-  const guests = document.getElementById("guests");
-  const quality = document.getElementById("quality");
-  const tipAmount = document.getElementById("tip-amount");
+const amount = document.querySelector("#amount");
+const guests = document.querySelector("#guests");
+const quality = document.querySelector("#quality");
+const tipAmount = document.querySelector("#tip-amount");
 
 calculate = () => {
   const tip = ((amount.value * quality.value) / (guests.value)).toFixed(2);
+  amount.value = "";
+  guests.value = "";
+  quality.value = "";
   if(tip === "NaN"){
-    tipAmount.innerHTML = "Tip $0 each";
+    tipAmount.textContent = "Tip $0 each";
     showTipAmount();
   }
   else{
-    tipAmount.innerHTML = "Tip $" + tip + " each";
+    tipAmount.textContent = "Tip $" + tip + " each";
     showTipAmount();
   }
 }
 
 showTipAmount = () => {
   // Get the snackbar DIV
-  var x = document.getElementById("tip-amount");
+  var x = document.querySelector("#tip-amount");
   // Add the "show" class to DIV
   x.className = "show";
   // After 3 seconds, remove the show class from DIV
